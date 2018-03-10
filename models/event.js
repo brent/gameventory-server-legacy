f3bf07df54eaf561ca54547728c33c4d905169c7
brent <brent.e.meyer@gmail.com>
2018-03-10 18:40:35 -0500

    Updates Event model schema

--- models/event.js
+++ models/event.js
@@ -5,11 +5,16 @@ mongoose.Promise = global.Promise;
 
 const eventSchema = new mongoose.Schema({
   // TODO: reevaluate whether these all need to be strings
-  actor: String,
-  actorUsername: String,
-  target: String,
-  type: String,
-  message: String
+  actor: {
+    id:       String,
+    username: String,
+  },
+  target: {
+    obj: String,
+    id:   String,
+    name: String
+  },
+  type: String
 });
 
 module.exports = mongoose.model('Event', eventSchema);
